647d2641fa4095eaa511091479c7a55f8256fe0a
Avanzo-Cyber-Security-Solutions <avanzonet@gmail.com>
2025-11-29 13:25:50 +0530

    changed baseurl

--- lib/apputils.dart
+++ lib/apputils.dart
@@ -2,5 +2,5 @@ class AppUtils {
   static String baseURL = "https://cybot.avanzosolutions.in/hms";
   // static String pythonBaseURL = "http://192.168.0.124:8000/hmsapp/api"; //naveen
   static String pythonBaseURL =
-      "http://192.168.220.61:8000/patientapp/api"; //midhun
+      "https://hms.highlandhospitals.in/api/patientapp/api"; //midhun
 }
